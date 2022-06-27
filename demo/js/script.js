50bcb8968d47d5d45d21018d99c6ebf176a7cb8d
wirabakti39 <wirabakti022@gmail.com>
2022-06-27 22:30:53 +0800

    update equal button

--- demo/js/script.js
+++ demo/js/script.js
@@ -5,37 +5,38 @@ buttons.forEach(function(btn){
     btn.addEventListener('click', () => {
 
         // ambil value dari atribut data pada tag button
-        let dataValue = btn.attributes.getNamedItem('data-btn').value;
+        const dataValue = btn.attributes.getNamedItem('data-btn').value;
 
-
-        if (dataValue=="*") {
-            inputBox.value += '×';
-        }
-        else if (dataValue=="/") {
-            inputBox.value += '÷';
-        }
-        else if (dataValue=="backspace") {
+        if (dataValue=="backspace") {
             // Hapus angka terakhir dari inputan
             const newValue = backspace(inputBox.value);
             inputBox.value = newValue;
         }
         else if(dataValue=="clear") {
-            console.log('button clear');
             // Kosongkan tabel inputan
             inputBox.value = "";
         }
-        else if (dataValue=="equal") {
-            console.log('button equal');
-            // 
-        }
-        else{
+        else if(dataValue=="equal") {
+            // Operasi hitung pada inputBox dijalankan
+            if (inputBox.value==='') {
+                inputBox.value = '';
+            } else {
+                try {
+                    // Dihitung menggunakan bantuan fungsi eval() wkwk.
+                    inputBox.value = eval(inputBox.value);
+                } catch (err) {
+                    // Muncul pop-up alert jika input user tidak sesuai / tdk bisa dijalankan
+                    alert(err);
+                }
+            }
+        } else{
             inputBox.value += dataValue;
         }
     });
 });
 
 function backspace(inValue) {
-    let lstNum = inValue.slice(-1);
-    const nilai = inValue.replace(lstNum, "");
-    return nilai;
+    const arr = Array.from(inValue); // ubah string menjadi char dlm bentuk array
+    const newValue = inValue.slice(0, (arr.length - 1)); // slice index terakhir dari array tadi
+    return newValue;
 }
